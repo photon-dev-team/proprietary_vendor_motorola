61d9c0fae6321b3bcc9795a04b926e8ce515d65b
justin <jokersax@gmail.com>
2012-01-19 07:29:24 -0500

    adding olympus

---
+++ olympus/olympus-vendor.mk
@@ -0,0 +1,21 @@
+
+# Live wallpaper packages
+PRODUCT_PACKAGES := \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    VisualizationWallpapers \
+    librs_jni
+
+# apps
+PRODUCT_PACKAGES := \
+    FileManager 
+
+# Publish that we support the live wallpaper feature.
+PRODUCT_COPY_FILES := \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+
+# Pick up overlay for features that depend on non-open-source files
+DEVICE_PACKAGE_OVERLAYS := vendor/moto/olympus/overlay
+
+$(call inherit-product, vendor/moto/olympus/olympus-vendor-blobs.mk)
